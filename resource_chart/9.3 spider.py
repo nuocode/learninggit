dacfb84b5d572ca395b70249ff572768db1cf08c
PromiseGo <nuoxumail@163.com>
2019-03-24 22:38:14 +0800

    spider sample

---
+++ resource_chart/9.3 spider.py
@@ -0,0 +1,15 @@
+from bs4 import BeautifulSoup
+import requests
+
+html = requests.get("https://www.douban.com/").text
+soup = BeautifulSoup(html,"html.parser")
+# print(soup.prettify())
+
+#找到第一个p标签
+# first_para = soup.find("p")
+# print(first_para)
+# para_title = soup.title
+# print(para_title)
+# print("第一个p节点class内容：",soup.p["class"])
+# print("所有a节点：",soup.find_all("a"))
+print(soup.text)
